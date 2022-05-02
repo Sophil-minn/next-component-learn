3206de8c64459b0be771afaa7eb0cd8822a12939
minmin <1056640663@qq.com>
2022-05-02 17:43:39 +0800

    feat: react jsx  实战

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Index from './pages/Index';
 import List from './pages/List';
+import JsxDemo from './pages/jsxDemo';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" default element={<Home />}></Route>
         <Route path="/index" element={<Index />}></Route>
         <Route path="/list" element={<List />}></Route>
+        <Route path="/jsxDemo" element={<JsxDemo />}></Route>
         <Route path="/home" element={<Home />}></Route>
       </Routes>
     </div>  
---
+++ src/pages/jsxDemo/CustomButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CustomButton(props) {
+  return (
+    <span className={props.color}>CustomButton</span>
+  )
+}
