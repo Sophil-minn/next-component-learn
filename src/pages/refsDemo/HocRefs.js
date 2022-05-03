ab155cf7149cf201ba5d02632d376bfa148adb73
minmin <1056640663@qq.com>
2022-05-03 21:56:22 +0800

    feat: 在 DevTools 中显示自定义名称

--- src/pages/refsDemo/HocRefs.js
+++ src/pages/refsDemo/HocRefs.js
@@ -15,10 +15,22 @@ function logProps(WrappedComponent) {
       return <WrappedComponent ref={forwardedRef} {...rest}/>;
     }
   }
-  return React.forwardRef((props, ref) => {
+
+  function forwardRefMinn(props, ref) {
     return <LogProps {...props} forwardedRef={ref} />;
-  });
+  }
+
+  // 在 DevTools 中为该组件提供一个更有用的显示名。
+  // 例如 “ForwardRef(logProps(MyComponent))”
+  const name = Component.displayName || Component.name;
+  console.log(name, 'name', Component.displayName, 'Component.displayName', Component.name);
+  forwardRefMinn.displayName = `minn(Hui)`;
+  console.log(forwardRefMinn.displayName, 'forwardRefMinn.displayName');
+  // 使用 React.forwardRef API 明确地将 refs 转发到内部的 FancyButton 组件。
+  // React.forwardRef 接受一个渲染函数，其接收 props 和 ref 参数并返回一个 React 节点
+  return React.forwardRef(forwardRefMinn);
 }
+
 
 class FancyButton extends Component {
   constructor(props){
@@ -62,6 +74,13 @@ class HocRefs extends Component {
   render(){
     return (
       <div>
+        {/* refs 将不会透传下去。这是因为 ref 不是 prop 属性。就像 key 一样，其被 React 进行了特殊处理。
+        如果你对 HOC 添加 ref，该 ref 将引用最外层的容器组件，而不是被包裹的组件。
+        // 我们导入的 FancyButton 组件是高阶组件（HOC）LogProps。
+        // 尽管渲染结果将是一样的，
+        // 但我们的 ref 将指向 LogProps 而不是内部的 FancyButton 组件！
+        // 这意味着我们不能调用例如 ref.current.focus() 这样的方法
+        */}
         <NewFancyButton 
           title={this.state.title} 
           onClick={this.handleClickFancyButton}
